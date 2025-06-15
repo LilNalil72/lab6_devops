6021a810f5e6d06c91092042484749d08b36b65c
LilNalil72 <nailsabirov2004@gmail.com>
2025-06-15 22:40:42 +0500

    V6_migrate

--- db_migrations/V6__add_gender_to_patients.sql
+++ db_migrations/V6__add_gender_to_patients.sql
@@ -1,3 +1,5 @@
-ALTER TABLE patients ADD COLUMN gender VARCHAR(1);
+INSERT INTO patients (full_name, policy_number)
+VALUES ('Гойда Сергей Романович', '2345678901');
+
 INSERT INTO schema_version (version) VALUES (6);
 
