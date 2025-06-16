655625700424f50a13704757703a3dfdfb299e89
LilNalil72 <nailsabirov2004@gmail.com>
2025-06-16 09:51:42 +0500

    Add doctor

--- db_migrations/V5__add_info_in_tables.sql
+++ db_migrations/V5__add_info_in_tables.sql
@@ -1,14 +1,11 @@
 INSERT INTO doctors (full_name, specialization)
-VALUES ('Иванов Петр Сергеевич', 'Терапевт');
+VALUES ('Горда Сергей Романович', 'Уролог'
 
 INSERT INTO patients (full_name, policy_number)
 VALUES ('Сидорова Мария Ивановна', '1234567890');
 
 INSERT INTO patients (full_name, policy_number)
 VALUES ('Гойда Сергей Романович', '1234567891');
-
-INSERT INTO doctors (full_name, specialization)
-VALUES ('Сабиров Наиль Ильдусович', 'Гинеколог');
 
 INSERT INTO appointments (doctor_id, patient_id, appointment_time)
 VALUES (1, 1, '2023-12-01 14:30:00');
